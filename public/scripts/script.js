738452d1a84dfd20d715acd96a9fff8fd3c412e4
Akash <akashtripathiak04@gmail.com>
2024-07-16 19:59:26 +0530

    Updated index.html

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,18 +4,29 @@ var config = {
   cKey: "IL9rJUN5pdFp1SAz8T7h3A==zsZkE8LwblsFdzLE",
 };
 
-var selectCity = document.getElementById("inputBox").value.toUpperCase();
-console.log(selectCity);
-var url = config.cUrl + selectCity;
-console.log(url);
-function loadCities() {
-  fetch(url, {
-    headers: {
-      "X-Api-Key": config.cKey,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
-}
+const searchBar = document.getElementById("searchBar");
+
+searchBar.addEventListener("keyup", (e) => {
+  console.log(e);
+  const city = e.target.value;
+  if (city) {
+    fetch(config.cUrl + city, {
+      headers: {
+        "X-Api-Key": config.cKey,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+});
+
+//price selector
+var slider = document.getElementById("priceRange");
+var output = document.getElementById("priceDisplay");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
